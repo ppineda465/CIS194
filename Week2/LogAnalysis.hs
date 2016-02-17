65e50de569da5f8b404a967baa87f7a2ce2411ea
Pamela Pineda Freire <ppineda@stackbuilders.com>
2016-02-17 15:21:43 -0500

    indented and sintaxis error

--- Week2/LogAnalysis.hs
+++ Week2/LogAnalysis.hs
@@ -3,21 +3,22 @@ module LogAnalysis where
 import Log
 ----------Exercise 1-------------------
 parseMessage :: String -> LogMessage
-parseMessage = arr.words
-where parseMessage ("I":times:mesg)= LogMessage Info (read times) $ unwords mesg
-      parseMessage ("W":times:mesg)= LogMessage Warning (read times) $ unwords mesg
-      parseMessage ("E":error:times:mesg)= LogMessage (Error (read error)) $ times unwords mesg
-      parseMessage n= Unknown $ unwords n
+parseMessage a = case words a of
+          ("I":times:mesg) -> LogMessage Info (read times) $ unwords mesg
+          ("W":times:mesg) -> LogMessage Warning (read times) $ unwords mesg
+          ("E":errors:times:mesg) -> LogMessage (Error (read errors)) (read times) $ unwords mesg
+          _ -> Unknown a
 
 parse :: String -> [LogMessage]
 parse = map parseMessage.lines
 ----------Exercise 2-------------------
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _) tree = tree
-insert nlog Leaf = Node Leaf nlog Leaf
-insert nlog (Node left log right)
-  | getTimeStamp nlog > getTimeStamp log  = Node left log (insert nlog right)
-  | getTimeStamp nlog <= getTimeStamp log = Node (insert nlog left) log right
+insert lm@(LogMessage _ ts _) (Node left lm'@(LogMessage _ ts' _) right)
+    | ts < ts'  = Node (insert lm left) lm' right
+    | otherwise = Node left lm' (insert lm right)
+
+insert lm Leaf  = Node Leaf lm Leaf
+insert _  mt    = mt
 
 ----------Exercise 3-------------------
 build :: [LogMessage] -> MessageTree
